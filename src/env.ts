3cc07c04746248ddebf7438f6656ff253a022634
Isaac <isaac.hauser@live.com.au>
2023-01-17 11:35:07 +1000

    Update env vars

--- src/env.ts
+++ src/env.ts
@@ -2,15 +2,15 @@ import { envsafe, str } from "envsafe";
 
 export const env = envsafe({
   AWS_ACCESS_KEY_ID: str(),
-  AWS_SECRET_ACCESS_KEY: str(),
-  AWS_S3_BUCKET: str(),
-  AWS_S3_REGION: str(),
-  BACKUP_DATABASE_URL: str({
-    desc: 'The connection string of the database to backup.'
+  AWS_ACCESS_SECRET: str(),
+  AWS_BUCKET: str(),
+  AWS_REGION: str(),
+  DATABASE_URL: str({
+    desc: "The connection string of the database to backup.",
   }),
   BACKUP_CRON_SCHEDULE: str({
-    desc: 'The cron schedule to run the backup on.',
-    default: '0 5 * * *',
-    allowEmpty: true
-  })
-})
+    desc: "The cron schedule to run the backup on.",
+    default: "0 5 * * *",
+    allowEmpty: true,
+  }),
+});
