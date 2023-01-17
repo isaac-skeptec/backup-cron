4aa1853a1c2e337627fec6c478ff593d4ac9917e
Isaac <isaac.hauser@live.com.au>
2023-01-17 11:40:28 +1000

    Fix envs

--- src/backup.ts
+++ src/backup.ts
@@ -4,12 +4,12 @@ import { createReadStream } from "fs";
 
 import { env } from "./env";
 
-const uploadToS3 = async ({ name, path }: {name: string, path: string}) => {
+const uploadToS3 = async ({ name, path }: { name: string; path: string }) => {
   console.log("Uploading backup to S3...");
 
-  const bucket = env.AWS_S3_BUCKET;
+  const bucket = env.AWS_BUCKET;
 
-  const client = new S3Client({ region: env.AWS_S3_REGION });
+  const client = new S3Client({ region: env.AWS_REGION });
 
   await client.send(
     new PutObjectCommand({
@@ -17,17 +17,17 @@ const uploadToS3 = async ({ name, path }: {name: string, path: string}) => {
       Key: name,
       Body: createReadStream(path),
     })
-  )
+  );
 
   console.log("Backup uploaded to S3...");
-}
+};
 
 const dumpToFile = async (path: string) => {
   console.log("Dumping DB to file...");
 
   await new Promise((resolve, reject) => {
     exec(
-      `pg_dump ${env.BACKUP_DATABASE_URL} -F t | gzip > ${path}`,
+      `pg_dump ${env.DATABASE_URL} -F t | gzip > ${path}`,
       (error, stdout, stderr) => {
         if (error) {
           reject({ error: JSON.stringify(error), stderr });
@@ -40,17 +40,17 @@ const dumpToFile = async (path: string) => {
   });
 
   console.log("DB dumped to file...");
-}
+};
 
 export const backup = async () => {
-  console.log("Initiating DB backup...")
+  console.log("Initiating DB backup...");
 
-  const timestamp = new Date().toISOString()
-  const filename = `backup-${timestamp}.tar.gz`
-  const filepath = `/tmp/${filename}`
+  const timestamp = new Date().toISOString();
+  const filename = `backup-${timestamp}.tar.gz`;
+  const filepath = `/tmp/${filename}`;
 
-  await dumpToFile(filepath)
-  await uploadToS3({name: filename, path: filepath})
+  await dumpToFile(filepath);
+  await uploadToS3({ name: filename, path: filepath });
 
-  console.log("DB backup complete...")
-}
+  console.log("DB backup complete...");
+};
